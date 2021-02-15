2abf6417d53a47f8cf202f8bce41612a12405f71
kardelen karataş <kardelen.karatas@gmail.com>
2021-02-15 11:14:40 +0100

    feat: add pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // npm modules
 import { useState, useEffect } from "react";
+import Pagination from "react-js-pagination";
 
 // internal modules
 import { marvelCharactersAPI as marvelCharactersApi } from "./api/marvelApi";
@@ -9,18 +10,35 @@ function App() {
   const maxCharactersInPage = 10;
 
   const [characters, setCharacters] = useState([]);
+  const [totalCharacters, setTotalCharacters] = useState(0)
   const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     marvelCharactersApi(maxCharactersInPage, currentPage * maxCharactersInPage)
       .then((response) => {
+        setTotalCharacters(response.data.total);
         setCharacters(response.data.results);
       })
       .catch((error) => console.log(error));
   }, [currentPage]);
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  } 
+
   return (
     <div className="App">
+
+      <div className="pagination">
+        <Pagination
+          activePage={currentPage}
+          itemsCountPerPage={maxCharactersInPage}
+          totalItemsCount={totalCharacters - maxCharactersInPage}
+          pageRangeDisplayed={5}
+          onChange={handlePageChange}
+        />
+      </div>
+
       <div className="cards">
         {characters.map((el) => {
           return (
@@ -30,6 +48,7 @@ function App() {
           );
         })}
       </div>
+
     </div>
   );
 }
