8acaef62affde1dc0847e568ee074e5aa23411ba
kardelen karataş <kardelen.karatas@gmail.com>
2021-02-15 10:44:22 +0100

    feat: parse marvel data and show character images on screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import './App.css';
+// npm modules
+import { useState, useEffect } from "react";
+
+// internal modules
+import { marvelCharactersAPI as marvelCharactersApi } from "./api/marvelApi";
+import "./App.css";
 
 function App() {
+  const maxCharactersInPage = 10;
+
+  const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    marvelCharactersApi(maxCharactersInPage, currentPage * maxCharactersInPage)
+      .then((response) => {
+        setCharacters(response.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, [currentPage]);
+
   return (
     <div className="App">
+      <div className="cards">
+        {characters.map((el) => {
+          return (
+            <div className="card">
+              <img src={el.thumbnail.path + "." + el.thumbnail.extension}></img>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
