91916be47bed56bcb509b1059fc31f2b5178177f
kardelen karataş <kardelen.karatas@gmail.com>
2021-02-16 15:18:08 +0100

    feat: add loading page

--- src/App.js
+++ src/App.js
@@ -15,24 +15,26 @@ function App() {
   const [totalCharacters, setTotalCharacters] = useState(0);
   const [name, setName] = useState("");
   const [currentPage, setCurrentPage] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
-    let params = {limit: maxCharactersInPage};
+    let params = { limit: maxCharactersInPage };
 
     name
-      ? ( params.name = name )
+      ? (params.name = name)
       : (params.offset = currentPage * maxCharactersInPage);
 
     marvelCharactersApi(params)
       .then((response) => {
         setTotalCharacters(response.data.total);
         setCharacters(response.data.results);
+        setLoading(false);
       })
       .catch((error) => console.log(error));
   }, [currentPage, name]);
 
   const handlePageChange = (pageNumber) => {
+    setLoading(true);
     setCurrentPage(pageNumber);
   };
 
@@ -42,36 +44,46 @@ function App() {
 
   return (
     <div className="App">
-      <div className="searchbar">
-        <input
-          type="text"
-          placeholder="Marvel Character's Name"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <img src="./search.svg"></img>
-      </div>
+      {loading ? (
+        <div className="loading">
+          <h3>Loading</h3>
+          <img src="./loading.gif" alt="loading"></img>
+        </div>
+      ) : (
+        <>
+          <div className="searchbar">
+            <input
+              type="text"
+              placeholder="Marvel Character's Name"
+              value={name}
+              onChange={handleNameChange}
+            />
+            <img src="./search.svg"></img>
+          </div>
+          <div className="pagination">
+            <Pagination
+              activePage={currentPage}
+              itemsCountPerPage={maxCharactersInPage}
+              totalItemsCount={totalCharacters - maxCharactersInPage}
+              pageRangeDisplayed={5}
+              onChange={handlePageChange}
+            />
+          </div>
 
-      <div className="pagination">
-        <Pagination
-          activePage={currentPage}
-          itemsCountPerPage={maxCharactersInPage}
-          totalItemsCount={totalCharacters - maxCharactersInPage}
-          pageRangeDisplayed={5}
-          onChange={handlePageChange}
-        />
-      </div>
-
-      <div className="cards">
-        {characters.map((el) => {
-          return (
-            <div className="card">
-              <img src={el.thumbnail.path + "." + el.thumbnail.extension}></img>
-              <h5>{el.name.toUpperCase()}</h5>
-            </div>
-          );
-        })}
-      </div>
+          <div className="cards">
+            {characters.map((el) => {
+              return (
+                <div className="card">
+                  <img
+                    src={el.thumbnail.path + "." + el.thumbnail.extension}
+                  ></img>
+                  <h5>{el.name.toUpperCase()}</h5>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
