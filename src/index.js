f9c509ebd8033f005a7e8ee07dae7d22d0f57b54
kardelen karataş <kardelen.karatas@gmail.com>
2021-02-16 20:56:08 +0100

    style: add marvel font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './fonts/EarthsMightiest-A3wg.otf'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
